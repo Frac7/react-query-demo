2d9a4be6e56b9a048d9c513297fb056d6f826143
francescacella <francescacella96@gmail.com>
2021-05-22 09:18:09 +0200

    Add infinite scroll example

--- src/components/Content/utils.js
+++ src/components/Content/utils.js
@@ -1,12 +1,21 @@
 import UserList from '../UserList';
+import InfiniteUserList from '../InfiniteUserList';
 import SingleUser from '../SingleUser';
 
-export const sections = [{
+export const sections = [
+  {
     key: '0',
     title: 'User List',
     component: <UserList />,
-},{
+  },
+  {
     key: '1',
+    title: 'Infinite User List',
+    component: <InfiniteUserList />,
+  },
+  {
+    key: '2',
     title: 'Single User',
     component: <SingleUser />,
-}];
+  },
+];
---
+++ src/components/InfiniteUserList/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Row, Col, Card, List, Avatar, Spin, Alert, Button } from 'antd';
+import { useInfiniteQuery } from 'react-query';
+
+export default () => {
+  const {
+    isLoading,
+    error,
+    data,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    'UserInfiniteList',
+    ({ pageParam = 1 }) =>
+      fetch(`https://reqres.in/api/users?page=${pageParam}&delay=5`).then(res =>
+        res.json(),
+      ),
+    {
+      getNextPageParam: currentPage =>
+        currentPage.page === currentPage.total_pages
+          ? undefined
+          : currentPage.page + 1,
+    },
+  );
+
+  const handleClick = () => fetchNextPage();
+
+  if (isLoading) {
+    return (
+      <Row align="middle" justify="center">
+        <Col>
+          <Spin />
+        </Col>
+      </Row>
+    );
+  }
+
+  if (error) {
+    return <Alert message={error} type="error" />;
+  }
+
+  console.log(hasNextPage);
+
+  return (
+    <Card
+      actions={[
+        hasNextPage && (
+          <Button onClick={handleClick} loading={isFetchingNextPage}>
+            Load More
+          </Button>
+        ),
+      ]}
+    >
+      {data.pages.map(page => (
+        <List
+          key={page.page}
+          itemLayout="horizontal"
+          dataSource={page.data}
+          renderItem={item => (
+            <List.Item key={item.id}>
+              <List.Item.Meta
+                avatar={<Avatar src={item.avatar} />}
+                title={`${item.first_name} ${item.last_name}`}
+                description={item.email}
+              />
+            </List.Item>
+          )}
+        />
+      ))}
+    </Card>
+  );
+};
