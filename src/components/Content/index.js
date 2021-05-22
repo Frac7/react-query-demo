54c76557a24854a648d226d82685fee0b5463a1c
francescacella <francescacella96@gmail.com>
2021-05-22 08:14:29 +0200

    Add container title

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,16 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { Col, Layout, Row } from 'antd';
+import { Col, Layout, Row, Typography } from 'antd';
 
 import { sections } from './utils';
 
 const { Content } = Layout;
+const { Title } = Typography;
 
 export default ({ section }) => (
   <Content>
+    <Row>
+      <Col offset={6}>
+        <Title>{sections[section].title}</Title>
+      </Col>
+    </Row>
     <Row>
       <Col xs={12} offset={6}>
         {sections[section].component}
       </Col>
     </Row>
   </Content>
-)
+);
